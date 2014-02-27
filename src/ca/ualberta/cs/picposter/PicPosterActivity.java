e84da7ee0b2d7797ec403d9b17ae64ea623b8967
cfudge <cfudge@ualbeta.ca>
2014-02-27 16:02:57 -0700

    Continued working on lab tutorial and got nowhere XD

--- src/ca/ualberta/cs/picposter/PicPosterActivity.java
+++ src/ca/ualberta/cs/picposter/PicPosterActivity.java
@@ -1,20 +1,39 @@
 package ca.ualberta.cs.picposter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Type;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import ca.ualberta.cs.picposter.controller.PicPosterController;
+import ca.ualberta.cs.picposter.model.PicPostModel;
 import ca.ualberta.cs.picposter.model.PicPosterModelList;
 import ca.ualberta.cs.picposter.view.PicPostModelAdapter;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class PicPosterActivity extends Activity {
 
+	private HttpClient httpclient = new DefaultHttpClient();
+	private Gson gson = new Gson();
 
 	public static final int OBTAIN_PIC_REQUEST_CODE = 117;
 
@@ -73,12 +92,87 @@ public class PicPosterActivity extends Activity {
 	}
 
 
-	public void searchPosts(View view) {
-		String searchTerm = this.searchPostsEditText.getText().toString();
+	public void searchPosts(View view) throws ClientProtocolException, IOException {
+		final String searchTerm = this.searchPostsEditText.getText().toString();
 		
 		//TODO : perform search, update model, etc
+		//Used the example at https://github.com/rayzhangcl/ESDemo/blob/master/ESDemo/src/ca/ualberta/cs/CMPUT301/chenlei/ESClient.java:
+		
+		Thread thread = new Thread() {
+			 public void run() {
+				 try {
+					 runOnUiThread(new Runnable() {
+
+						 @Override
+						 public void run() {
+							HttpGet searchRequest = null;
+							try
+							{
+								searchRequest = new HttpGet("http://cmput301.softwareprocess.es:8080/testing/_search?pretty=1&q=" + java.net.URLEncoder.encode(searchTerm,"UTF-8"));
+							} catch (UnsupportedEncodingException e)
+							{
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							HttpResponse response = null;
+							try
+							{
+								response = httpclient.execute(searchRequest);
+							} catch (ClientProtocolException e)
+							{
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} catch (IOException e)
+							{
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+
+							 String status = response.getStatusLine().toString();
+
+							 Log.e("Server Response", status);
+
+							 String json = null;
+							try
+							{
+								json = getEntityContent(response);
+							} catch (IOException e)
+							{
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+
+							 Type elasticSearchSearchResponseType = new TypeToken<ElasticSearchSearchResponse<PicPostModel>>(){}.getType();
+							 ElasticSearchSearchResponse<Recipe> esResponse = gson.fromJson(json, elasticSearchSearchResponseType);
+							 Log.e("Server Response", esResponse.toString());
+							 }
+						 });
+					 Thread.sleep(300);
+				 } catch (InterruptedException e) {
+					 e.printStackTrace();
+				 }
+			 };
+};
+		thread.start();
+		
+//		searchRequest.releaseConnection();
 		
 		this.searchPostsEditText.setText(null);
 		this.searchPostsEditText.setHint(R.string.search_posts_edit_text_hint);
+	}
+	
+	//Used the example at https://github.com/rayzhangcl/ESDemo/blob/master/ESDemo/src/ca/ualberta/cs/CMPUT301/chenlei/ESClient.java:
+	String getEntityContent(HttpResponse response) throws IOException {
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader((response.getEntity().getContent())));
+		String output;
+		System.err.println("Output from Server -> ");
+		String json = "";
+		while ((output = br.readLine()) != null) {
+			System.err.println(output);
+			json += output;
+		}
+		System.err.println("JSON:"+json);
+		return json;
 	}
 }
